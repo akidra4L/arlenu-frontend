dd7666a711e42fa47412a1285f4383380a206120
Alikhan Gubayev <AlikhanGubayev@MacBook-Air-Air.local>
2022-07-04 14:57:19 +0600

    arlenu-frontend

--- src/components/content/main/index.jsx
+++ src/components/content/main/index.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import tshirt from "../../../assets/shirt.jpg";
 
 export const Main = () => {
   return (
-    <div className="main bg-black">
+    <div className="main">
       <div className="main-wrapper">
-        <div className="main-wrapper-content">Hello, World!</div>
+        <div className="main-wrapper-content">
+          <img src={tshirt} alt="tshirt" />
+        </div>
+        <div className="main-wrapper-content">
+          <img src={tshirt} alt="tshirt" />
+        </div>
+        <div className="main-wrapper-content">
+          <img src={tshirt} alt="tshirt" />
+        </div>
+        <div className="main-wrapper-content">
+          <img src={tshirt} alt="tshirt" />
+        </div>
+        <div className="main-wrapper-content">
+          <img src={tshirt} alt="tshirt" />
+        </div>
+        <div className="main-wrapper-content">
+          <img src={tshirt} alt="tshirt" />
+        </div>
       </div>
     </div>
   );
