3339c45a9024a0852b84e1e856f015159d3a8e5b
Alikhan Gubayev <AlikhanGubayev@MacBook-Air-Air.local>
2022-07-08 18:02:38 +0600

    arlenu-frontend

--- src/components/content/main/index.jsx
+++ src/components/content/main/index.jsx
@@ -1,34 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import URL from "../../../config/url";
 import tshirt from "../../../assets/shirt.jpg";
 
 export const Main = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL).then((response) => {
+      setItems(response.data);
+    });
+  }, []);
+
   return (
     <div className="main">
       <div className="main-wrapper">
-        <div className="main-wrapper-content">
-          <img src={tshirt} alt="tshirt" />
-          <p>@nickname</p>
-        </div>
-        <div className="main-wrapper-content">
-          <img src={tshirt} alt="tshirt" />
-          <p>@nickname</p>
-        </div>
-        <div className="main-wrapper-content">
-          <img src={tshirt} alt="tshirt" />
-          <p>@nickname</p>
-        </div>
-        <div className="main-wrapper-content">
-          <img src={tshirt} alt="tshirt" />
-          <p>@nickname</p>
-        </div>
-        <div className="main-wrapper-content">
-          <img src={tshirt} alt="tshirt" />
-          <p>@nickname</p>
-        </div>
-        <div className="main-wrapper-content">
-          <img src={tshirt} alt="tshirt" />
-          <p>@nickname</p>
-        </div>
+        {items.map((item) => (
+          <div className="main-wrapper-content">
+            <img src={tshirt} alt="tshirt" />
+            <div className="main-wrapper-content-user">
+              <div className="main-wrapper-content-user-nickname">
+                @{item.nickname}
+              </div>
+              <div className="main-wrapper-content-user-title">
+                {item.title}
+              </div>
+            </div>
+            <div className="main-wrapper-content-text">
+              {item.description}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
