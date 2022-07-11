b3418ff8840a29373dd2c1a425218a2db54128a4
Alikhan Gubayev <AlikhanGubayev@MacBook-Air-Air.local>
2022-07-11 20:12:52 +0600

    arlenu-frontend

--- src/components/content/main/index.jsx
+++ src/components/content/main/index.jsx
@@ -6,20 +6,20 @@ import Like from "../../../icons/like.js";
 export const Main = () => {
   const [items, setItems] = useState([]);
 
-  // const [like, setLike] = useState({
-  //   likes: "",
-  // });
-  // const [isLiked, setIsLiked] = useState(false);
+  const [like, setLike] = useState({
+    likes: "",
+  });
+  const [isLiked, setIsLiked] = useState(false);
 
-  // const handleLike = (id) => {
-  //   if (isLiked) {
-  //     setIsLiked(false);
-  //     setLike(like - 1);
-  //   } else {
-  //     setIsLiked(true);
-  //     setLike(like + 1);
-  //   }
-  // };
+  const handleLike = (id) => {
+    if (isLiked) {
+      setIsLiked(false);
+      setLike(like - 1);
+    } else {
+      setIsLiked(true);
+      setLike(like + 1);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,7 +33,7 @@ export const Main = () => {
     <div className="main">
       <div className="main-wrapper">
         {items.map((item) => (
-          <div className="main-wrapper-content">
+          <div className="main-wrapper-content" key={item._id}>
             <div className="main-wrapper-content-image">
               <img src={item.image} alt="image" />
             </div>
@@ -44,10 +44,10 @@ export const Main = () => {
               <div className="main-wrapper-content-user-like">
                 {item.likes}
                 <button
-                  // onClick={() => {
-                  //   handleLike(item._id);
-                  //   console.log("clicked", item._id);
-                  // }}
+                  onClick={() => {
+                    handleLike(item._id);
+                    console.log("clicked", item._id);
+                  }}
                 >
                   <Like />
                 </button>
