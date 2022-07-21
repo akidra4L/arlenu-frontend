01d7442cc462c3b3aad0e2a5543c6e4c17a3cfff
Alikhan Gubayev <AlikhanGubayev@MacBook-Air-Air.local>
2022-07-21 12:07:46 +0600

    arlenu-frontend

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,6 +1,15 @@
 import axios from "axios";
 import URL from "../config/url";
 
+const token = JSON.parse(localStorage.getItem("token")).token;
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${token}`,
+};
+
 export const getItems = () => axios.get(URL);
-export const createItem = (item) => axios.post(URL, item);
+export const createItem = (item) =>
+  axios.post(URL, item, {
+    headers,
+  });
 export const loginUser = (user) => axios.post(URL, user);
